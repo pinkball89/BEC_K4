0a1a0c7137d8431cf36ef2d6283966d2bb5fde5e
Thi Thuy Dung Truong <ttruong68@gatech.edu>
2024-04-12 17:43:02 -0400

    Done 15 Java exercises Apr 12-13

---
+++ Pre BE/Java/WelcomToJava - Copy.java
@@ -0,0 +1,27 @@
+/**  Input Format
+
+ There is no input for this challenge.
+
+ Output Format
+
+ You must print two lines of output:
+
+ Print Hello, World. on the first line.
+ Print Hello, Java. on the second line.
+ Sample Output
+
+ Hello, World.
+ Hello, Java.
+ **/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
